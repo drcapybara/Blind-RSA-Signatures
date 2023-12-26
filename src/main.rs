66e75a9ea9b11f389e6eae19f89da242fb1d909e
Dr. Capybara <dustinray313@gmail.com>
2023-12-25 22:15:06 -0800

    fix: impl logic

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use rsa::{PaddingScheme, RsaPrivateKey, RsaPublicKey, PublicKeyParts};
-use rand::{rngs::OsRng, Rng};
 use num_bigint::BigUint;
+use rand::{rngs::OsRng, Rng};
+use rsa::{PaddingScheme, PublicKeyParts, RsaPrivateKey, RsaPublicKey};
 
 fn main() {
     let mut rng = OsRng;
@@ -10,44 +10,53 @@ fn main() {
 
     let message = b"secret message";
 
-    // Alices public key
-    let e = BigUint::from_bytes_be(public_key.e().to_bytes_be().as_slice());
-    let n = BigUint::from_bytes_be(public_key.n().to_bytes_be().as_slice());
+    // Alice's public key
+    let e = dbg!(BigUint::from_bytes_be(public_key.e().to_bytes_be().as_slice()));
+    let n = dbg!(BigUint::from_bytes_be(public_key.n().to_bytes_be().as_slice()));
 
     // Sign the message
     let padding = PaddingScheme::new_pkcs1v15_sign(None);
-    let signature = private_key.sign(padding, &message[..]).expect("failed to sign message");
-    let s = BigUint::from_bytes_be(&signature);
-
-    // Encrypt the signature: v = s^e (mod n)
-    let v = s.modpow(&e, &n);
+    let signature = private_key
+        .sign(padding, &message[..])
+        .expect("failed to sign message");
+    let s = dbg!(BigUint::from_bytes_be(&signature));
 
     // Number of iterations 'k'
     let k = 20;
 
     for i in 0..k {
         // Prover picks a random r_i
-        let r_i = BigUint::from(rng.gen::<u64>());
-        let c_i = r_i.modpow(&e, &n);
+        let r_i = dbg!(BigUint::from(rng.gen::<u32>()));
+        let c_i = dbg!(r_i.modpow(&e, &n));
 
         // Challenger picks a random bit b_i
-        let b_i = rng.gen::<bool>();
+        let b_i = dbg!(rng.gen::<bool>());
 
         // Prover's response
-        let z_i = if b_i {
+        let z_i = dbg!(if b_i {
             // Prover sends c_z_i = (s * r_i)^e (mod n)
-            s.modpow(&BigUint::from(1_u64), &n) * r_i.modpow(&BigUint::from(1_u64), &n)
+            (s.clone() * r_i.clone()) % &n
         } else {
             // Prover sends r_i
-            r_i
-        };
+            r_i.clone()
+        });
 
         // Verifier's verification
         if b_i {
-            let c_z_i = z_i.modpow(&e, &n);
-            assert_eq!(c_z_i, v.clone() * c_i % &n, "Verification failed at iteration {}", i);
+            let c_z_i = dbg!(z_i.modpow(&e, &n));
+            assert_eq!(
+                c_z_i,
+                dbg!((s.modpow(&e, &n) * c_i) % &n),
+                "Verification failed at iteration {}",
+                i
+            );
         } else {
-            assert_eq!(z_i.modpow(&e, &n), c_i, "Verification failed at iteration {}", i);
+            assert_eq!(
+                dbg!(z_i.modpow(&e, &n)),
+                c_i,
+                "Verification failed at iteration {}",
+                i
+            );
         }
     }
 
